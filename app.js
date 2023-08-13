be580dfaa8a642c3a333646a25472a9a9ba0760d
anderson <andersong.salvador@gmail.com>
2023-08-13 13:36:07 -0300

    att app

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const app = express()
-app.listen(3000)
+app.listen(9000)
 const venom = require('venom-bot');
 let image = '';
 venom
     .create({
         session: 'session-gabriel', //name of session
-        multidevice: true,
-        logQR:false,
-        useChrome:false,
-        debug:false,
-        devtools:false,
-        disableSpins:true,
-        disableWelcome:true,
-        updatesLog:false
-         // for version not multidevice use false.(default: true)
-    },(statusSession, session) => {
+        multidevice: false,
+        logQR: false,
+        useChrome: false,
+        debug: false,
+        devtools: false,
+        disableSpins: true,
+        disableWelcome: true,
+        updatesLog: false
+        // for version not multidevice use false.(default: true)
+    }, (statusSession, session) => {
         image = statusSession;
         // console.log('Status Session: ', statusSession);
         //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
         //Create session wss return "serverClose" case server for close
         // console.log('Session name: ', session);
-      })
+    })
     .then((client) => start(client))
     .catch((erro) => {
         console.log(erro);
@@ -29,9 +29,9 @@ venom
 
 
 
-    app.get('/', async function (req, res) {
-        res.send(`${image}`)
-    })
+app.get('/', async function (req, res) {
+    res.send(`${image}`)
+})
 
 
 
@@ -42,14 +42,14 @@ venom
 function start(client) {
     client.onMessage((message) => {
         if (message.isGroupMsg === false) {
-          client
-            .sendText(message.from, '🤖 Sou o boot do gabriel ele esta almoçando no momento.')
-            .then((result) => {
-            //   console.log('Result: ', result); //return object success
-            })
-            .catch((erro) => {
-            //   console.error('Error when sending: ', erro); //return object error
-            });
+            client
+                .sendText(message.from, '🤖 Sou o boot do gabriel ele esta almoçando no momento.')
+                .then((result) => {
+                    //   console.log('Result: ', result); //return object success
+                })
+                .catch((erro) => {
+                    //   console.error('Error when sending: ', erro); //return object error
+                });
         }
-      });
+    });
 }
